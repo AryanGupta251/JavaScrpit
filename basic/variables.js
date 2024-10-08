2dc50eb9cd11b425ec41a3abbe613c1db816270c
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-08 17:03:08 +0000

    second commit

---
+++ basic/variables.js
@@ -0,0 +1,7 @@
+ const accountID =144553;
+ let accountEmail="aryangupta86505@gmail.com"
+ var accountPassword = "1234"
+ accountCity="Budaun"
+let accountState;
+ console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+ 
