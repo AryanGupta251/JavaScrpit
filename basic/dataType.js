20d7be4679f52ab5952820814da7dc84eb6251c8
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-09 04:57:12 +0000

    data type

---
+++ basic/dataType.js
@@ -0,0 +1,36 @@
+"use strict";  // treat all js code as new version
+// alert(3+3)  // we are using nodejs , not browser
+
+
+/*
+// premitive datatype
+number
+bigint
+string
+boolean
+null=> standalone value 
+undefined
+symbol=>unique
+
+*/
+
+
+// console.log(42 / +0); // Infinity 
+// console.log(42 / -0); // -Infinity
+
+
+let name="Aryan"  //String
+let num=3333     // number
+let bool=true     //boolean
+let yes=null      //standalone value 
+let big =383828303973n //big int
+
+console.table([name,num,bool,yes,big]);
+console.log(name[2]);      // we can access any element stored in any index of string using its index
+
+
+
+
+
+
+
