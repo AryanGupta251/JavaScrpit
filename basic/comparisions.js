43bc32d9eb54c295f70ab858319b41f60246d029
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-09 14:29:34 +0000

    comparision

---
+++ basic/comparisions.js
@@ -0,0 +1,22 @@
+/*
+// this comparision returns boolean  value
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+*/
+
+
+/*console.log(null>0);    
+console.log(null==0);
+console.log(null>=0);   // treated as zero in non strict comparision (>=)
+
+console.log(undefined ==0);
+console.log(undefined>0);    
+console.log(undefined>=0);
+*/
+console.log("2"==2); // check only value for comparision  
+console.log("2"===2); // check datatype as well 
+
+
