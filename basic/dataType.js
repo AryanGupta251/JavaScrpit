26e471bb4efa76a2fdfbffb4cd711180805a894e
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-09 15:48:37 +0000

    data type summary

--- basic/dataType.js
+++ basic/dataType.js
@@ -24,13 +24,32 @@ let num=3333     // number
 let bool=true     //boolean
 let yes=null      //standalone value 
 let big =383828303973n //big int
-
-console.table([name,num,bool,yes,big]);
+let useremail;   //undefined 
+console.table([name,num,bool,yes,big,useremail]);
 console.log(name[2]);      // we can access any element stored in any index of string using its index
 
 
+let con1=Symbol("123")
+let con2=Symbol("123")
+console.log(con1==con2);
 
 
+
+// reference (non primitive)
+// Array , objects, Functions
+
+const heros =["shatiman","thor","naagraj"]  //array
+{
+    name:"aryan"
+    age:23                    //these are objects it is made under curly braces and written in key pair values
+    roll:12
+    branch:"computer engineering"
+}
+
+const myfun=function(){           // functions
+    console.log("Hello World!");
+    
+}
 
 
 
