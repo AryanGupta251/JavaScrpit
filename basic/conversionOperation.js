7f3cb92cb6ffb314c0913d9d3ed2ceeb5f269772
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-09 05:42:36 +0000

    conversion operation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,69 @@
+// type conversion
+let nam="Aryan"
+let value=Number(nam)
+// console.log(typeof value);
+// console.log(value); // NaN => not a number 
+
+
+let nm=undefined
+let conversion=Boolean(nm)
+// console.log(typeof conversion);
+// console.log(conversion) 
+
+
+  let c1=0       // 1 =true ; 0=> false
+  let c2=""       // "" =false ; "aeyah"=>true
+  let c3=null       // null=>false
+  let c4=undefined      // undefined=>false
+  let c5=5      // anynumber except  0=>true
+
+//   let con=Boolean(c2)
+//   console.log(con);
+  
+  
+                  
+let check=33
+let StrNum =String(check)
+// console.log(StrNum);
+// console.log(typeof StrNum);
+
+
+
+// .................................Operations................................
+ 
+let val=3
+let negval=-val
+// console.log(negval);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/3);
+console.log(2**2);
+*/
+
+let str1="hello"
+let str2="aryan"
+let str3=str1+str2
+// console.log(str3);
+
+/*
+console.log("1" +2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+*/
+// prefix,postfix in  js
+
+let a= 32
+let b=++a     //first value is increased and then assigned to variable "b"
+console.log(b);  //ouput => 33
+
+
+let c= 15
+let d=c++     //first value is assigned to variable "b" and then increased by one
+console.log(d);  //ouput => 15
+
+
+  
