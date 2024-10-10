79d9cbddd83ca904915647758e73bda2a2329b30
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-10 06:58:31 +0000

    String

---
+++ basic/String.js
@@ -0,0 +1,43 @@
+const str="Aryan"
+const repo="javascrpit"       // we do not use this method in todays tech world this is out-dated
+// console.log(name+repo);
+// console.log(str.concat(repo)); //concatenate function 
+
+
+
+// we use backticks in today tech world to add strings
+
+// console.log(`Hi this is ${name} and I'm here to make you aware about new syntax for adding strings using backtick in ${repo}`);  // String interpolation
+const gamename=new String('Aryan gupta')
+// console.log(gamename.length);  
+// console.log(gamename.toLowerCase());
+// console.log(gamename.indexOf('y'));
+// console.log(gamename.charAt(3));
+// console.log(gamename.lastIndexOf("Gupta"));
+// console.log(gamename.replace('y','t'));
+// console.log(gamename.search("a"));
+// console.log(gamename.split(","));
+
+
+const nw = gamename.substring(1,4)  //we can not use negative indexing in substring method 
+// console.log(nw);
+
+
+const nw1 = gamename.slice(-1,4)  // we can use negative index as well in slicing 
+// console.log(nw1);
+
+
+
+const arr="    hello     "
+// console.log(arr);
+// console.log(arr.trim());   // remove space 
+
+
+
+// read all method of string (home work)
+// console.log(gamename.substr(0,3));
+
+
+const str2 = "Hello";
+console.log(str2.toLowerCase()); // Output: "hello"
+console.log(str2.toUpperCase()); // Output: "HELLO"
