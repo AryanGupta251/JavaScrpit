cc53005e5c835303ee668cb6d1b7f9ccbd41927e
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-10-12 18:13:56 +0000

    date

---
+++ basic/datesin.js
@@ -0,0 +1,19 @@
+let mydate= new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());  // exject date
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString()); // month/date/year
+// console.log(mydate.toLocaleString());  // month/date/year   time
+// console.log(mydate.toUTCString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toJSON());
+// console.log(mydate.getTimezoneOffset());
+// console.log(typeof (mydate));  // object
+
+
+//different dates formate
+// let mycurrentdate = new Date(2024,9,12)     //year ,month,date
+// let mycurrentdate = new Date(2024,9,12) 
+// let mycurrentdate = new Date(2024,9,12) 
+// let mycurrentdate = new Date(2024,9,12) 
+console.log(mycurrentdate.toDateString());
