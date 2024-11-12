9b6673da87484dc425f23edb47ee57df2628cf99
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-11-12 03:51:14 +0000

    updated

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,19 +1,23 @@
+/*
+let i = 0
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i = i + 2
+}
+*/
 
-let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
 
-let myArray = ['flash', "batman", "superman"]
+/*
+let myArray = ['flash', "batman", "superman","Aryan","Anamta"]
 
 let arr = 0
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
+    console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
+*/
 
-let score = 11
+let score = 10
 
 do {
     console.log(`Score is ${score}`);
