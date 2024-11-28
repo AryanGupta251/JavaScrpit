edd298d2396fa24acdb3f66606b3e42d15b492dc
Aryan Gupta <130988967+AryanGupta251@users.noreply.github.com>
2024-11-28 16:54:16 +0000

    math

--- basic/maths.js
+++ basic/maths.js
@@ -13,3 +13,5 @@ console.log(Math.floor(Math.random()*10)+1); //  generate random value greater t
 const min=10;
 const max=20;
 console.log(Math.floor(Math.random()*(max-min +1))+min);   //   generate random value greater then 10 with only one digit
+
+// update
